a15a7a83fdaee5e6fd380739c02288393c14df11
gaganVirk <deep.virk@outlook.co.nz>
2019-07-15 20:36:09 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpingOnCloudsAlgorithm
+{
+    class Program
+    {
+        static int JumpingOnClouds(int[] c)
+        {
+            int counter = 0;
+
+            for(int i = 0; i < c.Length; i++)
+            {
+                if(c[i] == 0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp));
+            int result = JumpingOnClouds(c);
+
+            Console.WriteLine(result);
+        }
+    }
+}
